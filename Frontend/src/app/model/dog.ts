ad58d992cf9b993c7ebd425550918d27307dc400
Angelo <mateljohnangelo@gmail.com>
2022-10-22 20:10:49 +0800

    Lowercased mapping URLs, implement backend methods

---
+++ Frontend/src/app/model/dog.ts
@@ -0,0 +1,11 @@
+export class Dog {
+	dog_id: Number = 0
+	name: string = ''
+	gender: string = ''
+	age: string = ''
+	size: string = ''
+	color: string = ''
+	images: Blob[] = []
+	adjectives: string = ''
+	hasOwner: boolean = false
+}
---
+++ Frontend/src/app/service/adoption.service.ts
@@ -0,0 +1,36 @@
+import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Adoption } from '../model/adoption';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdoptionService {
+  apiUrl: string
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = 'http://localhost:18080/api';
+  }
+
+  public getAdoption(id: number): Observable<Adoption> {
+    return this.http.get<Adoption>(this.apiUrl + '/adoption/' + id.toString());
+  }
+
+  public getAdoptions(): Observable<Adoption[]> {
+    return this.http.get<Adoption[]>(this.apiUrl + '/adoptions/');
+  }
+
+  public addAdoption(adoption: Adoption): Observable<Adoption> {
+    return this.http.post<Adoption>(this.apiUrl + '/add-adoption/', adoption);
+  }
+
+  public updateAdoption(id: Number, adoption: Adoption): Observable<Adoption> {
+    return this.http.put<Adoption>(this.apiUrl + '/update-adoption/' + id, adoption);
+  }
+
+  public deleteAdoption(id: Number): Observable<Adoption> {
+    return this.http.delete<Adoption>(this.apiUrl + '/delete-adoption/' + id);
+  }
+}
