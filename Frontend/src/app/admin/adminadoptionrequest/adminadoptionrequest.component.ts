34eebf0bd6c38a887f084ca939f24e344ae1d4e7
Angelo <mateljohnangelo@gmail.com>
2022-10-26 02:09:03 +0800

    Added functionality to adoption request list

--- Frontend/src/app/admin/adminadoptionrequest/adminadoptionrequest.component.ts
+++ Frontend/src/app/admin/adminadoptionrequest/adminadoptionrequest.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Adoption } from 'app/model/adoption';
+import { AdoptionService } from 'app/service/adoption.service';
+
 @Component({
   selector: 'app-adminadoptionrequest',
   templateUrl: './adminadoptionrequest.component.html',
   styleUrls: ['./adminadoptionrequest.component.css']
 })
 export class AdminAdoptionRequestComponent implements OnInit {
+  adopters: Adoption[] = [];
 
-  constructor() { }
+  constructor(
+    private adoptionService: AdoptionService,
+  ) {}
 
   ngOnInit(): void {
+    this.adoptionService.getAdoptions().subscribe((adoptions) => {
+      this.adopters = adoptions;
+      console.log(adoptions)
+    });
   }
 
 }
